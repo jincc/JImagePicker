8d98d88826e2c3cc674fc28aae1d779f76db8aeb
Jincc <Jincc@jiangjunlongdeMacBook-Pro.local>
2015-09-11 15:18:00 +0800

    Initial Commit

---
+++ JImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JImagePicker
+//
+//  Created by 江钧龙 on 15/9/11.
+//  Copyright (c) 2015年 江钧龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JImagePicker
+//
+//  Created by 江钧龙 on 15/9/11.
+//  Copyright (c) 2015年 江钧龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
